d4376dd09abff2538fb28e921d0d9f9a8b4fc871
Caoimhinv <caoimhinvallely@gmail.com>
2021-04-14 10:02:55 +0100

    reorder columns

--- analysis.py
+++ analysis.py
@@ -78,6 +78,9 @@ I26 = ("standard deviation for virginica " + str(virginica[['totals']].std()) + 
 with open('outputted_iris_data_textfile.txt','w') as d_a:
     d_a.writelines([I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, I11, I12, I13, I14, I15, I16, I17,
                     I18, I19, I20, I21, I22, I23, I24, I25, I26])
+                    
+# reorders columns so we have 'species' last as before
+iris2 = iris2[['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'totals', 'mean', 'species']]
 
 # writes updated csv file with totals and means per row
 # we were getting lots of decimal places for some of the totals for some reason? 
